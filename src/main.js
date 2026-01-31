9d526d32948ea3ffcfdfc8e968d168e2f85acadf
zengin0201 <aleksejborodin711@gmail.com>
2026-01-31 10:09:00 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,44 @@
+/**
+ * Функция для расчета выручки
+ * @param purchase запись о покупке
+ * @param _product карточка товара
+ * @returns {number}
+ */
+function calculateSimpleRevenue(purchase, _product) {
+   // @TODO: Расчет выручки от операции
+}
+
+/**
+ * Функция для расчета бонусов
+ * @param index порядковый номер в отсортированном массиве
+ * @param total общее число продавцов
+ * @param seller карточка продавца
+ * @returns {number}
+ */
+function calculateBonusByProfit(index, total, seller) {
+    // @TODO: Расчет бонуса от позиции в рейтинге
+}
+
+/**
+ * Функция для анализа данных продаж
+ * @param data
+ * @param options
+ * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
+ */
+function analyzeSalesData(data, options) {
+    // @TODO: Проверка входных данных
+
+    // @TODO: Проверка наличия опций
+
+    // @TODO: Подготовка промежуточных данных для сбора статистики
+
+    // @TODO: Индексация продавцов и товаров для быстрого доступа
+
+    // @TODO: Расчет выручки и прибыли для каждого продавца
+
+    // @TODO: Сортировка продавцов по прибыли
+
+    // @TODO: Назначение премий на основе ранжирования
+
+    // @TODO: Подготовка итоговой коллекции с нужными полями
+}
